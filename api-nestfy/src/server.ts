3be6cf5720639a04ea53e096e408ed0f77504aee
Frankdias92 <frankmcdias@gmail.com>
2024-08-20 15:14:46 -0300

    setting ts setup

---
+++ api-nestfy/src/server.ts
@@ -0,0 +1,14 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', () => {
+    return 'hello'
+})
+
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('Server running!')
+})
