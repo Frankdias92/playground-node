3c89e003283d23e767e2226b349dcf8c0f9333e9
Frankdias92 <frankmcdias@gmail.com>
2024-08-22 22:17:40 -0300

    :sparkles: Feat: create project settings for deploy | all set

--- api-nestfy/src/database.ts
+++ api-nestfy/src/database.ts
@@ -3,10 +3,10 @@ import { env } from "./env";
 
 console.log('print env', env.DATABASE_URL)
 export const config: Knex.Config = {
-    client: 'sqlite3',
-    connection: {
-        filename: env.DATABASE_URL
-    },
+    client: env.DATABASE_CLIENT,
+    connection: env.DATABASE_CLIENT === 'sqlite' ? {
+            filename: env.DATABASE_URL
+        } : env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
         extension: 'ts',
