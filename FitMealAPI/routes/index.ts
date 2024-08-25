9278cb19ffbbbdeceaee11bfd1e66c161ba5ff17
Frankdias92 <frankmcdias@gmail.com>
2024-08-25 12:47:19 -0300

    :sparkles: Feat: setting database

--- FitMealAPI/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-
-
-export async function TransactionsRoutes(app: FastifyInstance) {
-    app.get('/', async (req, reply) => {
-        return reply.send('Welcome to app daily')
-    })
-}
