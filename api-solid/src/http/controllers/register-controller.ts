40de9621f2a8c7aeede8552c8f4a4a16ba48ffba
Frankdias92 <frankmcdias@gmail.com>
2024-09-18 18:53:07 -0300

    :package: Feat: applying interface db

--- api-solid/src/http/controllers/register-controller.ts
+++ api-solid/src/http/controllers/register-controller.ts
@@ -1,6 +1,8 @@
-import { registerUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repositorys'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
+import { RegisterUseCase } from '@/use-cases/register'
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import z from 'zod'
+import { z } from 'zod'
 
 export async function registerController(
   req: FastifyRequest,
@@ -15,13 +17,20 @@ export async function registerController(
   const { name, email, password } = registerBodySchema.parse(req.body)
 
   try {
-    await registerUseCase({
+    const userRepository = new PrismaUsersRepository()
+    const registerUserCase = new RegisterUseCase(userRepository)
+
+    await registerUserCase.execute({
       name,
       email,
       password,
     })
   } catch (error) {
-    return reply.status(409).send()
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
--- api-solid/src/use-cases/register.ts
+++ api-solid/src/use-cases/register.ts
@@ -1,6 +1,6 @@
-import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repositorys'
 import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import type { UsersRepository } from '@/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -8,28 +8,22 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private userRepository: UsersRepository) {}
 
-  const userWithSamaEmail = await prisma.user.findUnique({
-    where: {
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.userRepository.create({
+      name,
       email,
-    },
-  })
-
-  if (userWithSamaEmail) {
-    throw new Error('Email already in use')
+      password_hash,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
