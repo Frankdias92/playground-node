36c66bcf9adb9527edaf62badd02632851b4c0bd
Frankdias92 <frankmcdias@gmail.com>
2024-09-18 13:33:00 -0300

    :package: Feat: seting prisma to run on container and create a new user

--- api-solid/src/app.ts
+++ api-solid/src/app.ts
@@ -1,14 +1,29 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (req, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string(),
+    password: z.string(),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Franklin',
-    email: 'frank@email.com',
-    password_hash: '123456',
-  },
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
+
+app.get('/users', (req, reply) => {
+  return reply.send({ message: 'test get' })
 })
