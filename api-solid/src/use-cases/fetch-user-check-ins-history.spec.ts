ee4c9f4b532ea990a49a967bfe0c6d2a14251e72
Frankdias92 <frankmcdias@gmail.com>
2024-09-24 13:56:22 -0300

    :sparkles: Feat: search for gyms

--- api-solid/src/use-cases/fetch-user-check-ins-history.spec.ts
+++ api-solid/src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -1,16 +1,13 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
-import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
 import { FetchUserCheckInsUseCase } from './fetch-user-check-ins-history'
 
 let checkInsRepository: InMemoryCheckInRepository
-let gymsRepository: InMemoryGymsRepository
 let sut: FetchUserCheckInsUseCase
 
 describe('Fetch User Check-in History Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInRepository()
-    gymsRepository = new InMemoryGymsRepository()
     sut = new FetchUserCheckInsUseCase(checkInsRepository)
   })
 
@@ -38,7 +35,7 @@ describe('Fetch User Check-in History Use Case', () => {
   })
 
   it('should be able to fetch paginated history', async () => {
-    for (let i = 1; i <= 22; i) {
+    for (let i = 1; i <= 22; i++) {
       await checkInsRepository.create({
         gym_id: `gym-${i}`,
         user_id: 'user-01',
