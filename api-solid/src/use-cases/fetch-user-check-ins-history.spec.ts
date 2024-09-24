4aeb71ee248627795e77cc7f170e91b01b3229a2
Frankdias92 <frankmcdias@gmail.com>
2024-09-24 10:13:29 -0300

    :test_tube: Test: should be able to fetch user history

---
+++ api-solid/src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -0,0 +1,59 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { InMemoryCheckInRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { FetchUserCheckInsUseCase } from './fetch-user-check-ins-history'
+
+let checkInsRepository: InMemoryCheckInRepository
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchUserCheckInsUseCase
+
+describe('Fetch User Check-in History Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInRepository()
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchUserCheckInsUseCase(checkInsRepository)
+  })
+
+  it('should be able to fetch check-in history', async () => {
+    await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    await checkInsRepository.create({
+      gym_id: 'gym-02',
+      user_id: 'user-01',
+    })
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 1,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+
+  it('should be able to fetch paginated history', async () => {
+    for (let i = 1; i <= 22; i) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01',
+      })
+    }
+
+    const { checkIns } = await sut.execute({
+      userId: 'user-01',
+      page: 2,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
+    ])
+  })
+})
---
+++ api-solid/src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,27 @@
+import type { CheckIn } from '@prisma/client'
+import type { CheckInRespository } from '@/repositories/check-ins-repository'
+
+interface FetchUserCheckInsUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsUseCase {
+  constructor(private checkInsRepository: CheckInRespository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsUseCaseRequest): Promise<FetchUserCheckInsUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page
+    )
+
+    return { checkIns }
+  }
+}
