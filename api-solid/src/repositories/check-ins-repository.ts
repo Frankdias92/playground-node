d7e3fd4054e48cf430eff70935d737b1890dd460
Frankdias92 <frankmcdias@gmail.com>
2024-09-24 11:17:34 -0300

    :tube_test: Test: use case metrics

--- api-solid/src/repositories/check-ins-repository.ts
+++ api-solid/src/repositories/check-ins-repository.ts
@@ -2,6 +2,7 @@ import type { CheckIn, Prisma } from '@prisma/client'
 
 export interface CheckInRespository {
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
+  countByUserId(userId: string): Promise<number>
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
 }
