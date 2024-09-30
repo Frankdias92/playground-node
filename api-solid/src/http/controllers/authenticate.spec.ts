ddc4643c30f3d96bb2b198807a273d7c1c7ebd7c
Frankdias92 <frankmcdias@gmail.com>
2024-09-30 17:50:44 -0300

    :test_tube: generate new URL database envevionment

---
+++ api-solid/src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,12 @@
+import { app } from '@/app'
+import { beforeAll, describe, it } from 'vitest'
+
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    console.log('Before all')
+  })
+
+  it('should be able to authenticate', async () => {
+    console.log('controller authenticate')
+  })
+})
