796e4c2cf6f5642c6aefc7419130bce5b600be27
Frankdias92 <frankmcdias@gmail.com>
2024-10-01 16:12:24 -0300

    :tube_test: Test: add test e2e authenticate

--- api-solid/src/http/controllers/authenticate.spec.ts
+++ api-solid/src/http/controllers/authenticate.spec.ts
@@ -1,12 +1,31 @@
 import { app } from '@/app'
-import { beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Authenticate (e2e)', () => {
   beforeAll(async () => {
-    console.log('Before all')
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
   })
 
   it('should be able to authenticate', async () => {
-    console.log('controller authenticate')
+    await request(app.server).post('/users').send({
+      name: 'Franklin',
+      email: 'franklin@email.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'franklin@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
   })
 })
